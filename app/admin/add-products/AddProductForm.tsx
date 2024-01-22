a7557f1880e0799b57d7a686860fdb0ff981af6a
Ahmed Abdo <AhmedAbdo1612>
2024-01-22 21:20:00 +0200

    after adding drop zone

--- app/admin/add-products/AddProductForm.tsx
+++ app/admin/add-products/AddProductForm.tsx
@@ -1,14 +1,32 @@
 "use client";
 
 import Heading from "@/app/components/Heading";
+import CategoryInput from "@/app/components/Inputs/CategoryInput";
 import CustomCheckBox from "@/app/components/Inputs/CustomCheckBox";
 import Input from "@/app/components/Inputs/Inputs";
+import SelectColor from "@/app/components/Inputs/SelectColor";
 import TextArea from "@/app/components/Inputs/TextArea";
+import { categories } from "@/utils/Categories";
+import { colors } from "@/utils/colors";
 import { useState } from "react";
 import { FieldValues, useForm } from "react-hook-form";
 
+export type ImageType = {
+  color: string;
+  colorCode: string;
+  image: File | null;
+};
+
+export type UploadedImageType = {
+  color: string;
+  colorCode: string;
+  image: string;
+};
+
 const AddProductForm = () => {
   const [loading, setLoading] = useState(false);
+  const [images, setImages] = useState<ImageType[] | null>(null);
+  const [isProductCreated, setIsProductCreated] = useState(false);
   const {
     register,
     handleSubmit,
@@ -26,6 +44,14 @@ const AddProductForm = () => {
       price: "",
     },
   });
+  const category = watch("category");
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value, {
+      shouldValidate: true,
+      shouldDirty: true,
+      shouldTouch: true,
+    });
+  };
   return (
     <>
       <Heading title="Add a Product" center />
@@ -64,13 +90,56 @@ const AddProductForm = () => {
         errors={errors}
         required
       />
-      <CustomCheckBox id="inStock" register={register} 
-      label="This product is in stock "/>
+      <CustomCheckBox
+        id="inStock"
+        register={register}
+        label="This product is in stock "
+      />
       <div className="w-full font-medium">
         <div className="mb-2 font-semibold">Select a Category</div>
-        <div className="grid grid-cols-2 md:grid-cols-3 
-        max-h-[50vh] overflow-y-auto">
-
+        <div
+          className="grid grid-cols-2 md:grid-cols-3 
+        max-h-[50vh] overflow-y-auto gap-3"
+        >
+          {categories.map((item) => {
+            if (item.label === "All") {
+              return null;
+            }
+            return (
+              <div key={item.label} className="col-span-1">
+                <CategoryInput
+                  onClick={(category) => setCustomValue("category", category)}
+                  selected={category === item.label}
+                  label={item.label}
+                  icon={item.icon}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div className="w-full flex flex-col flex-wrap gap-4">
+        <div>
+          <div className="font-bold">
+            Select the available product colors and upload thier images
+          </div>
+          <div className="text-sm">
+            You must select an image for each color you select otherwise your
+            color selection will be ignored.
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-3">
+          {colors.map((item, index) => {
+            return (
+              <SelectColor
+                key={index}
+                item={item}
+                addImageToState={() => {}}
+                removeImageFromState={() => {}}
+                isProductCreated={isProductCreated}
+              />
+            );
+          })}
         </div>
       </div>
     </>
---
+++ app/components/Inputs/SelectColor.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import { ImageType } from "@/app/admin/add-products/AddProductForm";
+import { useCallback, useEffect, useState } from "react";
+import SelectImage from "./SelectImage";
+import Button from "../Button/Button";
+
+interface SelectColorProps {
+  item: ImageType;
+  addImageToState: (value: ImageType) => void;
+  removeImageFromState: (value: ImageType) => void;
+  isProductCreated: boolean;
+}
+
+const SelectColor: React.FC<SelectColorProps> = ({
+  item,
+  addImageToState,
+  removeImageFromState,
+  isProductCreated,
+}) => {
+  const [selected, setSelected] = useState(false);
+  const [file, setFile] = useState<File | null>(null);
+
+  useEffect(() => {
+    if (isProductCreated) {
+      setSelected(false);
+      setFile(null);
+    }
+  }, [isProductCreated]);
+  const handleFileChange = useCallback((value: File) => {
+    setFile(value);
+    addImageToState({ ...item, image: value });
+  }, []);
+  const handleCheck = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setSelected(e.target.checked);
+    if (!e.target.checked) {
+      setFile(null);
+      removeImageFromState(item);
+    }
+  }, []);
+  return (
+    <div
+      className="gird grid-cols-1 md:grid-cols-2 overflow-y-auto
+    border-b-[1.2px] border-slate-200 items-center p-2 "
+    >
+      <div className="flex flex-row gap-2 items-center h-[60px]">
+        <input
+          id={item.color}
+          type="checkbox"
+          checked={selected}
+          onChange={handleCheck}
+          className="cursor-pointer"
+        />
+        <label htmlFor={item.color} className="font-medium cursor-pointer">
+          {item.color}
+        </label>
+      </div>
+      <>
+        {selected && !file && (
+          <div className="col-span-2 text-center">
+            <SelectImage item={item} handleFileChange={handleFileChange} />
+          </div>
+        )}
+        {file && (
+          <div className="flex flex-row gap-2 text-sm col-span-2 items-center justify-between">
+            <p>{file?.name}</p>
+            <div className="w-[70px]">
+              <Button
+                small
+                outline
+                label="Cancel"
+                onClick={() => {
+                  setFile(null);
+                  removeImageFromState(item);
+                }}
+              />
+            </div>
+          </div>
+        )}
+      </>
+    </div>
+  );
+};
+export default SelectColor
